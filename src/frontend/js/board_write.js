2d58bc2059c7925f30b977507993a1d0e70d0677
clo-lpT <hama4342@naver.com>
2022-06-23 09:31:42 +0900

    2022.06.23 add submit button

--- src/frontend/js/board_write.js
+++ src/frontend/js/board_write.js
@@ -6,15 +6,15 @@ function board_write() {
     <div className='board_write'>
       <Header />
       <div className='write'>
-        제목
+        <span>제목</span>
         <input type='text'></input>
-        카테고리
+        <span>카테고리</span>
         <select name="category">
             <option value="">카테고리</option>
             <option value="자유게시판">자유게시판</option>
             <option value="투자결산">투자결산</option>
         </select>
-        내용
+        <span>내용</span>
         <input type='text'></input>
         <form method='post'>
           <div>
@@ -24,6 +24,14 @@ function board_write() {
           </div>
           <input type='file' id='chooseFile' name='chooseFile' accept='image/*'></input>
         </form>
+        <div className='submit_cancle'>
+          <div className='submit_button'>
+            <span>등록</span>
+          </div>
+          <div className='cancle_button'>
+            <span>취소</span>
+          </div>
+        </div>
       </div>
     </div>
   );
