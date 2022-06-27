656a9415ab4470b6dab832fb9b0fca1e781f841f
clo-lpT <hama4342@naver.com>
2022-06-27 17:24:24 +0900

    2022.06.27 design board_write css

--- server/nginx/frontend/src/pages/board_write.js
+++ server/nginx/frontend/src/pages/board_write.js
@@ -7,15 +7,15 @@ function board_write() {
       <Header />
       <div className='write'>
         <span>제목</span>
-        <input type='text' id='title' ></input>
+        <input type='text' id='title' className='board_input'></input>
         <span>카테고리</span>
-        <select name="category">
+        <select name="category" className='board_input'>
             <option value="">카테고리</option>
             <option value="자유게시판">자유게시판</option>
             <option value="투자결산">투자결산</option>
         </select>
         <span>내용</span>
-        <textarea id='content' />
+        <textarea id='content' className='board_input'/>
         <form method='post'>
           <div>
             <label for='chooseFile'>
