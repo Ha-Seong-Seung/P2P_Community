fa5a538defceba7237b68171208d00c6fb50315f
clo-lpT <hama4342@naver.com>
2022-07-05 16:12:43 +0900

    2022.07.05 modify chat page

--- server/nginx/frontend/src/pages/chat.js
+++ server/nginx/frontend/src/pages/chat.js
@@ -17,90 +17,104 @@ function Chat() {
 
   // 윈도우 사이즈 변경할 때마다 사이즈 조정
   window.addEventListener("resize", () => ScreenSize());
+
   return (
     <div className="chat" style={{ minHeight: windowHeight }}>
       <Header />
       <div className="my_chat">
         <div className="my_chat_list">
           <div className="chat_list_top">
-            <span>메시지</span>
+            <span>Message</span>
           </div>
           <div className="chat_list_contents">
             <div className="chat_other">
               <img src={profile} alt="profile" />
               <div className="chat_info">
-                <span>트롤리</span>
-                <span>채팅내용</span>
+                <div className="chat_contents">
+                  <span id="nickname">트롤리</span>
+                  <span>채팅내용dddddddddddddddd</span>
+                </div>
+                <span> 2022.12.08 12:08</span>
               </div>
             </div>
             <div className="chat_other">
               <img src={profile} alt="profile" />
               <div className="chat_info">
-                <span>트롤리</span>
-                <span>채팅내용dddddddddddddddd</span>
+                <div className="chat_contents">
+                  <span id="nickname">트롤리</span>
+                  <span>채팅내용dddddddddddddddd</span>
+                </div>
+                <span> 2022.12.08 12:08</span>
               </div>
             </div>
             <div className="chat_other">
               <img src={profile} alt="profile" />
               <div className="chat_info">
-                <span>트롤리</span>
-                <span>채팅내용dddddddddddddddd</span>
+                <div className="chat_contents">
+                  <span id="nickname">트롤리</span>
+                  <span>채팅내용dddddddddddddddd</span>
+                </div>
+                <span> 2022.12.08 12:08</span>
               </div>
             </div>
             <div className="chat_other">
               <img src={profile} alt="profile" />
               <div className="chat_info">
-                <span>트롤리</span>
-                <span>채팅내용dddddddddddddddd</span>
+                <div className="chat_contents">
+                  <span id="nickname">트롤리</span>
+                  <span>채팅내용dddddddddddddddd</span>
+                </div>
+                <span> 2022.12.08 12:08</span>
               </div>
             </div>
             <div className="chat_other">
               <img src={profile} alt="profile" />
               <div className="chat_info">
-                <span>트롤리</span>
-                <span>채팅내용dddddddddddddddd</span>
+                <div className="chat_contents">
+                  <span id="nickname">트롤리</span>
+                  <span>채팅내용dddddddddddddddd</span>
+                </div>
+                <span> 2022.12.08 12:08</span>
               </div>
             </div>
             <div className="chat_other">
               <img src={profile} alt="profile" />
               <div className="chat_info">
-                <span>트롤리</span>
-                <span>채팅내용dddddddddddddddd</span>
+                <div className="chat_contents">
+                  <span id="nickname">트롤리</span>
+                  <span>채팅내용dddddddddddddddd</span>
+                </div>
+                <span> 2022.12.08 12:08</span>
               </div>
             </div>
             <div className="chat_other">
               <img src={profile} alt="profile" />
               <div className="chat_info">
-                <span>트롤리</span>
-                <span>채팅내용dddddddddddddddd</span>
+                <div className="chat_contents">
+                  <span id="nickname">트롤리</span>
+                  <span>채팅내용dddddddddddddddd</span>
+                </div>
+                <span> 2022.12.08 12:08</span>
               </div>
             </div>
             <div className="chat_other">
               <img src={profile} alt="profile" />
               <div className="chat_info">
-                <span>트롤리</span>
-                <span>채팅내용dddddddddddddddd</span>
+                <div className="chat_contents">
+                  <span id="nickname">트롤리</span>
+                  <span>채팅내용dddddddddddddddd</span>
+                </div>
+                <span> 2022.12.08 12:08</span>
               </div>
             </div>
             <div className="chat_other">
               <img src={profile} alt="profile" />
               <div className="chat_info">
-                <span>트롤리</span>
-                <span>채팅내용dddddddddddddddd</span>
-              </div>
-            </div>
-            <div className="chat_other">
-              <img src={profile} alt="profile" />
-              <div className="chat_info">
-                <span>트롤리</span>
-                <span>채팅내용</span>
-              </div>
-            </div>
-            <div className="chat_other">
-              <img src={profile} alt="profile" />
-              <div className="chat_info">
-                <span>트롤리</span>
-                <span>채팅내용</span>
+                <div className="chat_contents">
+                  <span id="nickname">트롤리</span>
+                  <span>채팅내용dddddddddddddddd</span>
+                </div>
+                <span> 2022.12.08 12:08</span>
               </div>
             </div>
           </div>
